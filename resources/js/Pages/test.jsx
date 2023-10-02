671f6cd5bf75ebba83d060eb8f31afe0d2d6c9b0
mumtazalwan <mumtazalwan@gmail.com>
2023-10-02 15:07:16 +0700

    [AAM] set up breeze:react project

---
+++ resources/js/Pages/test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../../css/app.css';
+import { Link, Head } from '@inertiajs/react';
+
+export default function test(props) {
+    return(
+     <div>
+         <h1>Kita Mulai dari 0 yah</h1>
+     </div>
+    )
+}
