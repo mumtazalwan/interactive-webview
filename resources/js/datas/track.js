2bf569e7f39c72350b11a5f19054c0b94847ce4b
Antariksa3 <antariksakusumaharmawan6801@gmail.com>
2023-10-06 08:30:53 +0700

    feat: [AKH] add some music

--- resources/js/datas/track.js
+++ resources/js/datas/track.js
@@ -1,5 +1,8 @@
 import elevatorMusic from '../../../public/assets/audio/elevator-music.mp3'
 import ohTengoSuerte from '../../../public/assets/audio/oh-tengo-suerte.mp3'
+import todayDiary from '../../../public/assets/audio/today-diary.mp3'
+import inDreamland from '../../../public/assets/audio/in-dreamland.mp3'
+import honeyJam from '../../../public/assets/audio/honey-jam.mp3'
 
 export const tracks = [
     {
@@ -11,5 +14,20 @@ export const tracks = [
         title: 'Oh! Tengo Suerte',
         src: ohTengoSuerte,
         artist: 'Masayoshi Takanaka'
-    }
+    },
+    {
+        title: 'Today\'s Diary',
+        src: todayDiary,
+        artist: 'Chiro'
+    },
+    {
+        title: 'In Dreamland',
+        src: inDreamland,
+        artist: 'Chillpeach'
+    },
+    {
+        title: 'Honey Jam',
+        src: honeyJam,
+        artist: 'massobeats'
+    },
 ]
